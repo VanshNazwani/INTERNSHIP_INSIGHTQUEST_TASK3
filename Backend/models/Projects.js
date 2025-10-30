ede8ebb4f3d83c4a30e1162a9558d000ae5cda35
VanshNazwani <nazwaniv@gmail.com>
2025-10-31 00:53:23 +0530

    Delete Backend directory

--- Backend/models/Projects.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const projectSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-});
-
-export default mongoose.model("Project", projectSchema);
